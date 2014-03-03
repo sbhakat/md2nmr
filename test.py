4a7f3e7078c66042b28c06d6a5c31c9134949dc8
Oliver Schillinger <oliver@schillinger-ttp.de>
2014-03-03 09:50:12 +0100

    test.py modified

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ from md2nmr import *
 from NHcorr import *
 
 name = '04_prod01_protein'
-path = '/home/oliver/externalDisk/MD_BACKUP/IL-6/IL-6_ffcomp/amber99sb-star_2IL6_13'
+#path = '/home/oliver/externalDisk/MD_BACKUP/IL-6/IL-6_ffcomp/amber99sb-star_2IL6_13'
+path = '/home/oliver/SiSc/Courses/Thesis/iGRASP/remote_fs/gpufs/externalDisk/MD_BACKUP/IL-6/IL-6_ffcomp/amber99sb-star_2IL6_13'
 
 md = md2nmr(name, path=path)
+
 
 
 
